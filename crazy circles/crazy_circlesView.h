b69501044ee35d065e9b0c6ca9f38cb72ab9fdb1
ajsoriar <ajsoriar@gmail.com>
2015-09-27 10:21:28 +0200

    Initial Commit

---
+++ crazy circles/crazy_circlesView.h
@@ -0,0 +1,13 @@
+//
+//  crazy_circlesView.h
+//  crazy circles
+//
+//  Created by The man with no name on 27/09/2015.
+//  Copyright © 2015 ajsoriar. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface crazy_circlesView : ScreenSaverView
+
+@end
